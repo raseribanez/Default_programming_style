44a175e8025ba013bc1b9fea29876af524290f88
Ben Woodfield <raser.13@hotmail.com>
2016-11-13 17:01:42 +0000

    Create default_2.py

---
+++ default_2.py
@@ -0,0 +1,9 @@
+# This is the standard format, my other method is basically a neat version of this
+
+__author__ = "Ben Woodfield"
+__copyright__ = "Copyright (C) 2016 Ben Woodfield"
+__license__ = "Public Domain"
+__version__ = "1.0"
+
+# I may use this method from time to time but will try to eventually add
+# a Single layout to all of my previous and future programs written in Python
