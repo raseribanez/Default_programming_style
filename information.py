8e83c96d309aacdbada866870b850f286dbe404b
Ben Woodfield <raser.13@hotmail.com>
2016-11-13 17:35:52 +0000

    Create information.py

---
+++ information.py
@@ -0,0 +1,27 @@
+# THIS ISNT A PROGRAM
+# Just a visual demonstration of my formatting preferences across code
+#
+# THIS WONT APPLY SO STRICTLY OVER MY TUTORIAL CODING, AS i WANT READABILITY TO BE THE MOST IMPORTANT
+# THING, SOMETIMES A BIT OF EXTRA SPACING CAN MAKE THINGS EASIER ON THE EYE
+
+
+# FUNCTIONS
+def this_method(): # How I write function definitions
+
+# VARIABLES
+thisMethod = something # How I name Variables (No_Underscore - for easy recognition as a variable)
+
+# PRINTING
+print"This Method"  # Instead of print("This Method") Not using parentheses if not required
+
+# COMMENTS
+# Comments (#) No longer than 80 characters per line, written in proper, understandable English
+
+# Sentences start with Upper case char unless used to comment out a line of code
+
+# Removing code for testing purposes # Using this comment method with a brief line-above-comment explaining the codes purpose
+
+# PARENTHESES SPACING
+# (one,two three,four) and (1,2,3,4) indstead of (1, 2, 3, 4) so, without spaces
+
+# (10+10), not (10 + 10) 
