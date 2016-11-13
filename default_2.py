fa0d1578ac5a5df3f002a11bd9c7813fe2f5a851
Ben Woodfield <raser.13@hotmail.com>
2016-11-13 17:53:30 +0000

    added BIG project info

--- default_2.py
+++ default_2.py
@@ -5,5 +5,18 @@ __copyright__ = "Copyright (C) 2016 Ben Woodfield"
 __license__ = "Public Domain"
 __version__ = "1.0"
 
-# I may use this method from time to time but will try to eventually add
+
+# The longer way for this method is as follows, but this would only
+# be for a large project as it seems a bit overkill for small stuff
+
+__author__ = "Ben Woodfield"
+__copyright__ = (C)"Copyright 2016, Ben Woodfield "
+__credits__ = ["Audio", "Graphics", "Contributions"]
+__license__ = "GPL"
+__version__ = "1.0.1"
+__maintainer__ = "Ben Woodfield"
+__email__ = "raserppsprograms@gmail.com"
+__status__ = "Production"
+
+# I may use the first method from time to time but will try to eventually add
 # a Single layout to all of my previous and future programs written in Python
